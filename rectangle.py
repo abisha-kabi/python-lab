04ba02306514249523e28aaa21d91813f9719727
abisha-kabi <abhishakbabu2001@gmail.com>
2024-12-30 15:28:54 +0530

    Add files via upload

--- rectangle.py
+++ rectangle.py
@@ -1,25 +1,4 @@
-
-class Rectangle:
-   def __init__(self, length, breadth):
-        self.length = length
-        self.breadth = breadth
-   def area(self):
-       return (self.length * self.breadth)
-   def perimeter(self):
-      return 2 * (self.length + self.breadth)
-   def compare(self,other):
-       if self.length==other.length:
-           return "Same"
-       else:
-           return "different"
-r1=Rectangle(10,3)
-r2=Rectangle(10,3)
-r1.area()
-r1.perimeter()
-print("area and perimeter is:",r1.area(),r1.perimeter())
-r2.area()
-r2.perimeter()
-print("area and perimeter is:", r2.area(),r2.perimeter())
-print(r1.compare(r2))
-
-
+def area(h,w):
+    return h*w
+def perimeter(h,w):
+    return 2*(h+w)
